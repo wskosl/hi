4619a9c621bdf5c08cb6e838682fd2951268eaee
wskosl <skoslwu@gmail.com>
2025-07-04 13:24:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,21 @@
           alert("You didn't enter a name.");
         }
       }
-const map = L.map('map').setView([28.3949, 84.1240], 5);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(map);
- const tracePoints = [
-      [29.653, 91.117],   // Lhasa
-      [12.2958, 76.6394],  // Mysore
-      [32.219, 76.323]    // Dharamshala
-    ];
-    const pathLine = L.polyline(tracePoints, { color: 'red' }).addTo(map);
-    
-    map.fitBounds(pathLine.getBounds());
+document.addEventListener("DOMContentLoaded", function () {
+  const map = L.map('map').setView([28.3949, 84.1240], 5);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
 
-    L.marker(tracePoints[0]).addTo(map).bindPopup('Lhasa');
-    L.marker(tracePoints[1]).addTo(map).bindPopup('Mysore');
-    L.marker(tracePoints[2]).addTo(map).bindPopup('Dharamshala');
+  const tracePoints = [
+    [29.653, 91.117],   // Lhasa
+    [12.2958, 76.6394],  // Mysore
+    [32.219, 76.323]     // Dharamshala
+  ];
+  const pathLine = L.polyline(tracePoints, { color: 'red' }).addTo(map);
+  map.fitBounds(pathLine.getBounds());
+
+  L.marker(tracePoints[0]).addTo(map).bindPopup('Lhasa');
+  L.marker(tracePoints[1]).addTo(map).bindPopup('Mysore');
+  L.marker(tracePoints[2]).addTo(map).bindPopup('Dharamshala');
+});
